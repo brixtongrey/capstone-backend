0891f79ad8158c01edc3b865acb61ac1d6840689
Yesenia Ramirez <ramirezy0415@gmail.com>
2025-12-04 20:18:18 -0600

    feature: Include user as part of group

--- api/groups.js
+++ api/groups.js
@@ -3,14 +3,23 @@ const router = express.Router();
 export default router;
 
 import { createGroup } from "#db/queries/groups";
+import { createGroupMember } from "#db/queries/group_members";
+import requireUser from "#middleware/requireUser";
 import requireBody from "#middleware/requireBody";
+
+router.use(requireUser);
 
 router.post("/new", requireBody(["name", "description"]), async (req, res) => {
   try {
+    const { id } = req.user;
     const { name, description } = req.body;
+    console.log(id);
     const group = await createGroup(name, description);
 
     if (!group) res.status(500).send("Unable to create group");
+
+    const user = await createGroupMember(group.id, id);
+    console.log(user);
 
     return res.status(201).send(group);
   } catch (error) {
