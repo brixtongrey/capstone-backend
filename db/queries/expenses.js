e96cb3066aace5e6f30d371362c9560718bae70b
Brixtongrey <brixtonaruda@gmail.com>
2025-12-12 15:20:44 -0700

    feat: add query to getExpensesByUserId

--- db/queries/expenses.js
+++ db/queries/expenses.js
@@ -48,6 +48,7 @@ export async function getExpensesByUserId(user_id) {
       i.name AS item_name, 
       se.amount_owed AS item_amount,
       g.name AS group_name
+      g.id AS group_id,
       FROM expenses e
       JOIN items i ON e.item_id = i.id
       JOIN groups g ON e.group_id = g.id
